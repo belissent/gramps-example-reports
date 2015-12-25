bf4597828d39190d8aa21c9c1146be014921ad57
gramps_example_reports <gramps@example.com>
2015-12-25 10:04:59 +0000

    Deploy example reports to GitHub pages

--- report_build.js
+++ report_build.js
@@ -1,18 +1,18 @@
 builds = {
     "gramps42": {
         "addons/master": "342dcf0db3a5c621584541aec3f8cce4261848ca", 
-        "gramps-example-reports/master": "c8c6e38e5d11d2df406ff952f9adb0ffc642f72a", 
+        "gramps-example-reports/master": "fd600da6f09dc6232402bd6fbaed8f752b7527d6", 
         "gramps/gramps42": "6bca97820723716b47eceac2ac4acb7f93c2e53f", 
-        "travis_build_id": "98596766", 
-        "travis_build_number": "114", 
+        "travis_build_id": "98760248", 
+        "travis_build_number": "116", 
         "user": "belissent"
     }, 
     "gramps50": {
         "addons/master": "342dcf0db3a5c621584541aec3f8cce4261848ca", 
-        "gramps-example-reports/master": "c8c6e38e5d11d2df406ff952f9adb0ffc642f72a", 
+        "gramps-example-reports/master": "fd600da6f09dc6232402bd6fbaed8f752b7527d6", 
         "gramps/gramps50": "46ecff17cf89259ad6a09175a52266c04dd7765b", 
-        "travis_build_id": "98596766", 
-        "travis_build_number": "114", 
+        "travis_build_id": "98760248", 
+        "travis_build_number": "116", 
         "user": "belissent"
     }
 };
